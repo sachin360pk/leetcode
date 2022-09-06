7e3752b7e6495536acde0b1c5a7247fb0d1f5bb2
sachin360pk <94632352+sachin360pk@users.noreply.github.com>
2022-09-06 10:49:44 +0530

    Time: 2 ms (71.20%), Space: 41.6 MB (33.20%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -1,13 +1,8 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        //if a number is power of two it reduce to one if we 
-        //recursively divide it by two while the remainder is zero
-        //check if number is one
-        if(n==0)
-            return false;
-        while(n%2==0){
-             n/=2;
-        }
-        return n==1;
+       if(n==0)
+           return false;
+        long x=(long)n;
+        return (x & (x-1))==0;
     }
 }
