4994e622eb0e2bfe00e429ca928b1f2668e99f52
sachin360pk <94632352+sachin360pk@users.noreply.github.com>
2023-02-03 21:22:05 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (99.79%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,18 +1,46 @@
+// first method
+
+// class Solution {
+//     public int maxProduct(int[] nums) {
+//         if(nums.length==1){
+//             return nums[0];
+//         }
+//         int ans=Integer.MIN_VALUE;
+//         for(int i=0;i<nums.length-1;i++){
+//             int pro=nums[i];
+//             ans=Math.max(pro,ans);
+//             for(int j=i+1;j<nums.length;j++){
+//                 pro=pro*nums[j];
+//                 ans=Math.max(pro,ans);
+//             }
+//         }
+//         ans=Math.max(nums[nums.length-1],ans);
+//         return ans;
+//     }
+// }
+
+
+// second method
+
 class Solution {
     public int maxProduct(int[] nums) {
-        if(nums.length==1){
-            return nums[0];
-        }
-        int ans=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length-1;i++){
-            int pro=nums[i];
-            ans=Math.max(pro,ans);
-            for(int j=i+1;j<nums.length;j++){
-                pro=pro*nums[j];
-                ans=Math.max(pro,ans);
+        int res=Integer.MIN_VALUE;
+        int t1=1;
+        int t2=1;
+        for(int i=0;i<nums.length;i++){
+            t1 = t1*nums[i];
+            res = Math.max(res,t1);
+            if(t1==0){
+                t1 = 1;
             }
         }
-        ans=Math.max(nums[nums.length-1],ans);
-        return ans;
+        for(int i=nums.length-1;i>=0;i--){
+            t2 = t2*nums[i];
+            res = Math.max(res,t2);
+            if(t2==0){
+                t2=1;
+            }
+        }
+        return res;
     }
 }
